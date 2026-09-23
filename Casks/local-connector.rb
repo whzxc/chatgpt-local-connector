003fb2621575a9dfc9d8fcf121a5e6224a2c4086
whzxc <47264468+whzxc@users.noreply.github.com>
2026-09-24 00:39:03 +0800

    chore(release): 同步 0.8.1 安装包校验值

--- Casks/local-connector.rb
+++ Casks/local-connector.rb
@@ -1,6 +1,6 @@
 cask "local-connector" do
-  version "0.8.0"
-  sha256 "b1ffaf20c0c3cc471ec2c552db657bffd32735a605e557d69258a251dc83f967"
+  version "0.8.1"
+  sha256 "c0cc836e24361ae11cc633a8f1b3bc91123fced13a1c861d5a84161a424e38d9"
 
   url "https://github.com/whzxc/chatgpt-local-connector/releases/download/v#{version}/Local.Connector_#{version}_aarch64.dmg"
   name "Local Connector"
